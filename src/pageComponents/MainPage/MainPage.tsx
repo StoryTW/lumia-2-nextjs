652e6dce599dc8e9b1e26cc40567602dd46dc7c1
parienko_sergej <parienko_sergej@enot.work>
2024-06-30 02:20:09 +0300

    fix

--- src/pageComponents/MainPage/MainPage.tsx
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -17,24 +17,26 @@ export function MainPage() {
 
       <SectionDaCex />
 
-      {/* <SectionApi
+      <SectionApi
         id='three'
         title='Swap API'
         description='Gain a massive competitive edge by harnessing the untapped liquidity of CEXs, leveraging a seamless routing mechanism to ensure your users benefit from the most optimal liquidity routes.'
         video='4'
       />
+
       <SectionApi
         id='four'
         title='Order API'
         description='Beat the competition with simplified yet comprehensive order management for spot & perps markets with features like order tracking, history retrieval, execution routes, and more.'
         video='5'
       />
+
       <SectionApi
         id='five'
         title='Bridge API'
         description='Empower your users with the easiest, fastest, and most secure cross-chain bridging experience powered by a trustless, peer-to-peer environment.'
         video='6'
-      /> */}
+      />
 
       <SectionAccelerate />
 
